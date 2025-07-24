ae111181b1be74e606c064698e2c0a34d6e24381
Glen Rogers <wisco.barbell@gmail.com>
2025-07-24 12:11:44 -0500

    Fix App import extension in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import NutritionApp from './App';
+import NutritionApp from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<NutritionApp />);
