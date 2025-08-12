4dda9037c4de78db28faeaa0d4edc5b5420fe9c4
Glen <wisco.barbell@gmail.com>
2025-08-11 21:13:20 -0500

    Update Index for enhanced version

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import NutritionApp from './App.jsx';
+import NutritionApp from './EnhancedApp.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<NutritionApp />);
